286f07d0f6eed2707f87692528ed4cb3a3ec4892
Kuldip Rupsangbhai Parmar <116505382+PKR9759@users.noreply.github.com>
2023-02-19 23:27:26 +0530

    Update TernarySearch.cpp

--- TernarySearch.cpp
+++ TernarySearch.cpp
@@ -6,8 +6,8 @@ using namespace std;
 int main(){
     int arr[8]={1,3,6,8,9,12,23,45},target=23,pos=-2;
     int l=7,st=0,e=l;
-    int mid1=st+(st+((e-2*(st))/3)) ;// to avoid overflow --as binary search
-    int mid2=e-(st+((e-2*(st))/3));
+    int mid1=st+(st+((e-2*(st))/3)) ;        // to avoid overflow --as binary search
+    int mid2=e-(st+((e-2*(st))/3));     // you can use *** mid1= st+(e-s)/3;  mid2= e-(e-s)/3 ***; 
 
     while(st<e){
     	if(arr[mid1]==target){pos=mid1;break;}
