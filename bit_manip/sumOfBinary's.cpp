b6601238eed94cda0e45d12ff8e4fa847d15c6f3
Kuldip Rupsangbhai Parmar <116505382+PKR9759@users.noreply.github.com>
2023-05-05 20:58:39 +0530

    Create sumOfBinary's.cpp

---
+++ bit_manip/sumOfBinary's.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        StringBuilder res = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int carry = 0;
+        while(i >= 0 || j >= 0){
+            int sum = carry;
+            if(i >= 0) sum += a.charAt(i--) - '0';
+            if(j >= 0) sum += b.charAt(j--) - '0';
+            carry = sum > 1 ? 1 : 0;
+            res.append(sum % 2);
+        }
+        if(carry != 0) res.append(carry);
+        return res.reverse().toString();
+    }
+}
+//explain
+
+/*
+if sum=0   res=0   carry=0
+if sum=1   res=1   carry=0
+if sum=2   res=0   carry=1
+if sum=3   res=1   carry=1
+
+*/
